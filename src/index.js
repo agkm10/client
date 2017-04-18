b4a0eff3d03ad6525bbf32ee5a4ed4ee774c0719
Toby Farmer <farmertoby@gmail.com>
2017-04-18 12:50:35 -0500

    fixed minor duplicates

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 import './index.css';
-import store from "./store";
-
 import App from "./containers/App/index";
 import store from './store'
 import { Provider } from "react-redux";
