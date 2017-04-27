0471a79cd33f33b515ea54b72c8445f916ac72ab
Ben Biggs <bfletcherbiggs@gmail.com>
2017-04-27 14:20:24 -0500

    socket start

--- src/ducks/reducer.js
+++ src/ducks/reducer.js
@@ -1,20 +1,19 @@
-import { combineReducers } from "redux"
-import { reducer as formReducer } from 'redux-form'
-import clientDuck from './clientDuck'
-import loginDuck from './loginDuck'
-import messageDuck from './messageDuck'
-import appDuck from './appDuck'
-import inputDuck from './inputDuck'
-import uploadDuck from './uploadDuck'
-import authDuck from './authDuck'
+import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
+import clientDuck from './clientDuck';
+import socketDuck from './socketDuck';
+import messageDuck from './messageDuck';
+import inputDuck from './inputDuck';
+import uploadDuck from './uploadDuck';
+import authDuck from './authDuck';
+
 
 export default combineReducers({
-  clientDuck,
-  loginDuck,
-  messageDuck,
-  appDuck,
-  inputDuck,
-  uploadDuck,
-  authDuck,
-  form:formReducer
+    clientDuck,
+    socketDuck,
+    messageDuck,
+    inputDuck,
+    uploadDuck,
+    authDuck,
+    form:formReducer
 })
